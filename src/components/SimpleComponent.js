879b973e75bddbbd3a03d57c03237f6f7fe4f492
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-03-07 21:06:01 -0500

    Finished lab.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,20 @@
 // Code SimpleComponent Here
+import React, {Component} from 'react'
+
+export default class SimpleComponent extends Component{
+  state = {
+    mood: 'happy'
+  }
+
+  changeMood = () => {
+    this.setState(prevState => {
+      return prevState.mood === 'happy' ? {mood: 'sad'} : {mood: 'happy'}
+    })
+  }
+
+  render(){
+    return(
+      <div onClick={this.changeMood} >{this.state.mood}</div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const toggleMood = (e) => {
+  e.target.innerHTML = e.target.innerHTML === 'I am just happy.' ? 'I am just sad.' : 'I am just happy.'
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={toggleMood} />
   </div>,
   document.getElementById('root')
 );
